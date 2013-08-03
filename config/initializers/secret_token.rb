d93f0d3529dde28b9af74cb95b78f00c15681a05
Esteba Arango <marranoparael31@gmail.com>
2013-08-03 02:50:16 -0500

    Setting up everything.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrillBox::Application.config.secret_key_base = '7558a5053cf55ed8e469a40ef87499b8b24f3ce0613ce2b2c6a3daf22e9e6a810251c4bf3eeaea61c1005358209e6ab39a2f7d2e5ec52bb983e657367ff7cc4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrillBox::Application.config.session_store :cookie_store, key: '_DrillBox_session'
